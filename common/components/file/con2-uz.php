77818de6bb6405489d7ecc525dc9002993786532
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2024-09-20 14:46:20 +0500

    Update con2-uz.php

--- common/components/file/con2-uz.php
+++ common/components/file/con2-uz.php
@@ -208,14 +208,15 @@ $limg = $lqr->writeDataUri();
 
     <tr>
         <td colspan="4">
-            2.2. <?= $joy ?> Ushbu shartnoma bo‘yicha ta’lim oluvchini bir yillik o‘qitish uchun to‘lov 8,219,893.00 (sakkiz million ikki yuz o`n to`qqiz ming sakkiz yuz to`qson uch so'm) soʻmni tashkil etadi va quyidagi muddatlarda amalga oshiriladi: Choraklarga bo‘lib to‘langanda quyidagi muddatlarda:
+            2.2. <?= $joy ?> Ushbu shartnoma bo‘yicha ta’lim oluvchini bir yillik o‘qitish uchun to‘lov 8,219,893.00 (sakkiz million ikki yuz o`n to`qqiz ming sakkiz yuz to`qson uch so'm) soʻmni tashkil etadi va quyidagi muddatlarda amalga oshiriladi:
+            <br>Choraklarga bo‘lib to‘langanda quyidagi muddatlarda:
             <br>
             <ul>
                 <li>
-                    belgilangan to‘lov miqdorining kamida 25.00 foizini talabalikka tavsiya etilgan abiturientlar uchun 15.10.2024 gacha, ikkinchi va undan yuqori bosqich talabalar uchun 01.11.2024 gacha;
+                    <?= $joy ?><?= $joy ?>belgilangan to‘lov miqdorining kamida 25.00 foizini talabalikka tavsiya etilgan abiturientlar uchun 15.10.2024 gacha, ikkinchi va undan yuqori bosqich talabalar uchun 01.11.2024 gacha;
                 </li>
                 <li>
-                    belgilangan to‘lov miqdorining kamida 50.00 foizini 01.01.2025 gacha, 75.00 foizini 01.04.2025 gacha va
+                    <?= $joy ?><?= $joy ?>belgilangan to‘lov miqdorining kamida 50.00 foizini 01.01.2025 gacha, 75.00 foizini 01.04.2025 gacha va
                     100.00 foizini 01.06.2025 gacha.
                 </li>
             </ul>
