507dec487bc6ad4d444a78131585fa0763b14b5a
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2024-10-08 11:44:26 +0500

    1

--- backend/views/target/view.php
+++ backend/views/target/view.php
@@ -10,6 +10,23 @@ use Da\QrCode\QrCode;
 
 $this->title = $model->name;
 $user = Yii::$app->user->identity;
+$consId = $user->cons_id;
+$domen = 'qabul';
+if ($consId == 4) {
+    $domen = 'tashkent';
+} elseif ($consId == 5) {
+    $domen = 'edu';
+} elseif ($consId == 6) {
+    $domen = 'talim';
+} elseif ($consId == 7) {
+    $domen = 'uz';
+} elseif ($consId == 8) {
+    $domen = 'sam';
+} elseif ($consId == 9) {
+    $domen = 'uzedu';
+} elseif ($consId == 10) {
+    $domen = 'mahalla';
+}
 ?>
 <div class="target-view">
 
@@ -41,7 +58,7 @@ $user = Yii::$app->user->identity;
             <div class="row">
                 <div class="col-md-12 col-lg-6">
                     <?php
-                        $url = "https://qabul.sarbon.university?id=".$model->id;
+                        $url = "https://".$domen.".sarbon.university?id=".$model->id;
                     ?>
                     <h6 class="badge-table-div active"><?= $url ?></h6>
                     <div class="mt-3">
@@ -55,7 +72,7 @@ $user = Yii::$app->user->identity;
                 </div>
                 <div class="col-md-12 col-lg-6">
                     <?php
-                    $url2 = "https://qabul.sarbon.university/site/sign-up?id=".$model->id;
+                    $url2 = "https://".$domen.".sarbon.university/site/sign-up?id=".$model->id;
                     ?>
                     <h6 class="badge-table-div active"><?= $url2 ?></h6>
                     <div class="mt-3">
