1824062ea16e2b81ab7c014f3f1cac247448136f
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-01-27 11:51:02 +0500

    Update Message.php

--- common/models/Message.php
+++ common/models/Message.php
@@ -41,26 +41,26 @@ class Message extends \yii\db\ActiveRecord
                         }
                     ]
                 }';
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://send.smsxabar.uz/broker-api/send",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $data,
-            CURLOPT_HTTPHEADER => array(
-                "Authorization: Basic ".base64_encode("tgfu:M40KH237PwM]"),
-                "cache-control: no-cache",
-                "content-type: application/json",
-            ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-        return $phone." --- ".$response;
+//        $curl = curl_init();
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => "https://send.smsxabar.uz/broker-api/send",
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => "",
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 30,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => "POST",
+//            CURLOPT_POSTFIELDS => $data,
+//            CURLOPT_HTTPHEADER => array(
+//                "Authorization: Basic ".base64_encode("tgfu:M40KH237PwM]"),
+//                "cache-control: no-cache",
+//                "content-type: application/json",
+//            ),
+//        ));
+//        $response = curl_exec($curl);
+//        $err = curl_error($curl);
+//        curl_close($curl);
+//        return $phone." --- ".$response;
     }
 
 
@@ -83,25 +83,25 @@ class Message extends \yii\db\ActiveRecord
                         }
                     ]
                 }';
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://send.smsxabar.uz/broker-api/send",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $data,
-            CURLOPT_HTTPHEADER => array(
-                "Authorization: Basic ".base64_encode("tgfu:M40KH237PwM]"),
-                "cache-control: no-cache",
-                "content-type: application/json",
-            ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
+//        $curl = curl_init();
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => "https://send.smsxabar.uz/broker-api/send",
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => "",
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 30,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => "POST",
+//            CURLOPT_POSTFIELDS => $data,
+//            CURLOPT_HTTPHEADER => array(
+//                "Authorization: Basic ".base64_encode("tgfu:M40KH237PwM]"),
+//                "cache-control: no-cache",
+//                "content-type: application/json",
+//            ),
+//        ));
+//        $response = curl_exec($curl);
+//        $err = curl_error($curl);
+//        curl_close($curl);
         return $response;
     }
 
